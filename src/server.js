a44b370a7c98b05b0f6cd67dd6bf5bd3386d5a77
Krishna Varshney <commercekv2003@gmail.com>
2024-04-24 08:28:09 +0530

    change in the name of server file

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const dbHost = process.env.MONGODB_URI  || 'mongodb://localhost:27017/face_search';
-mongoose.connect(dbHost).then(() =>
+mongoose.connect("mongodb+srv://commercekv2003:20VSGx67QiXZUPMc@cluster0.lsbea0k.mongodb.net/facesearch?retryWrites=true&w=majority&appName=Cluster0").then(() =>
     console.log("Connected to the database!")).catch((err) =>
         console.log("Cannot connect to the database!", err)
     );
